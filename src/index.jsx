1a98d3a7cf1bf02f64aa5a8979c1f58643511c6c
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-06-28 00:17:40 +0200

    functional component and class component example running

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
+// FUNCTIONAL COMPONENT
+// - has only props (not changable)
+// - has no states!
+const Hello = (props) => {
+  return <div>Hello, {props.name} </div>;
 };
 
+// CLASS COMPONENT
+// - DONT FORGET TO ADAPT IMPORT LINE: import React, { Component } from react
+// - has props, state variables and class fucntions
+// - render() function is mandatory
+// - constructor is mandatory
+// - extends Component
+class Hello2 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { // defines initial state
+      clicked: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  }
+
+  render () {
+    // BUILD AND RENDER HTML
+    const addClass = this.state.clicked ? "clicked" : null;
+    return (
+      <div className={addClass + " greeting"}
+        onClick={this.handleClick}>
+        Hello {this.props.name}
+      </div>
+    )
+  }
+}
+
+
+// MAIN RENDER FUNCTION!
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="Thomas" />, root);
+  ReactDOM.render(
+    <Hello2 name="Thomas" />,
+    root
+  );
 }
+
+
