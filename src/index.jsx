2e53b4c232d399fc6e98b2cdacf94fbc1b83ed0b
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-06-28 01:38:44 +0200

    add css from other repo

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+import '../assets/stylesheets/coin-card.scss';
+import '../assets/stylesheets/header.scss';
 
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(<Hello name="Thomas" />, root);
 }
+
+
